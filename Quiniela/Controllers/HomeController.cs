c76d2062ce8ce148f96b6ced68a408c67f4b5249
C-Desktop <caor2.12@gmail.com>
2018-06-12 08:13:11 -0600

    Se añadio un buton en Index para guardar las predicciones de todos los usuarios en excel (C:\Uni-Quiniela\UQ_Results_All.xlsx).
    Falta:
    *enviar las predicciones generadas por correo.

--- Quiniela/Controllers/HomeController.cs
+++ Quiniela/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Data;
+using System.Reflection;
+using ClosedXML.Excel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -34,9 +38,7 @@ namespace Quiniela.Controllers
 
             return View(model);
         }
-
-
-
+        
         public ActionResult Prediction()
         {
             QuinielaGolEntities db = new QuinielaGolEntities();
@@ -58,7 +60,8 @@ namespace Quiniela.Controllers
             {
                 return Json(new { status = "Se alcanzó la fecha límite." }, JsonRequestBehavior.AllowGet);
             }
-            else {
+            else
+            {
                 if (alreadyUploaded)
                 {
                     var toDelete = db.Prediction.Where(x => x.UserId == UserId).ToList<Prediction>();
@@ -136,6 +139,91 @@ namespace Quiniela.Controllers
         }
 
         // Excel Area -----------------------------------------------------------------------------------
+
+        public void downloadPredictions()
+        {
+            QuinielaGolEntities db = new QuinielaGolEntities();
+            var wb = new XLWorkbook();
+            List<Prediction> predList = db.Prediction.ToList();
+            List<string> already = new List<string>();
+
+            if (predList.Count > 0)
+            {
+                int i = 4;
+                foreach (var item in predList) {
+
+                    string usrName = item.AspNetUsers.UserName;
+                    if(!already.Contains(usrName)){
+                        i = 4;
+                        already.Add(usrName);
+                        wb.Worksheets.Add(usrName);
+                        wb.Worksheet(usrName).Cell("B2").Value = usrName;
+                        wb.Worksheet(usrName).Cell("B3").Value = "Fecha";
+                        wb.Worksheet(usrName).Cell("C3").Value = "Local";
+                        wb.Worksheet(usrName).Cell("D3").Value = "GL";
+                        wb.Worksheet(usrName).Cell("E3").Value = "GV";
+                        wb.Worksheet(usrName).Cell("F3").Value = "Visitante";
+
+                        //Sheet Styles
+                        //Ranges
+                        var rngTable = wb.Worksheet(usrName).Range("B2:F51");
+                        var rngTitle = wb.Worksheet(usrName).Range("B2:F2");
+                        var rngDates = rngTable.Range("D3:D5"); // The address is relative to rngTable (NOT the worksheet)
+                        var rngNumbers = rngTable.Range("E3:E5"); // The address is relative to rngTable (NOT the worksheet)
+                        var rngHeaders = rngTable.Range("A2:E2"); // The address is relative to rngTable (NOT the worksheet)
+
+                        rngHeaders.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                        rngHeaders.Style.Font.Bold = true;
+                        
+                        //wb.Worksheet(usrName).Columns(1,5).AdjustToContents();
+                        rngTable.Style.Font.FontName = "Arial";
+                        rngTable.Style.Font.FontSize = 12;
+                        rngHeaders.Style.Font.FontSize = 14;
+                        rngHeaders.Style.Font.FontColor = XLColor.White;
+                        rngHeaders.Style.Fill.BackgroundColor = XLColor.Gray;
+
+
+                        rngTable.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+
+                        rngTable.Cell(1, 1).Style.Font.Bold = true;
+                        rngHeaders.Style.Font.FontSize = 16;
+                        rngTitle.Style.Font.FontColor = XLColor.White;
+                        rngTable.Cell(1, 1).Style.Fill.BackgroundColor = XLColor.SteelBlue;
+                        rngTable.Cell(1, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                        rngTable.Row(1).Merge(); // We could've also used: rngTable.Range("A1:E1").Merge()
+
+                        //Add a thick outside border
+                        rngTable.Style.Border.OutsideBorder = XLBorderStyleValues.Thick;
+
+                        // You can also specify the border for each side with:
+                        // rngTable.FirstColumn().Style.Border.LeftBorder = XLBorderStyleValues.Thick;
+                        // rngTable.LastColumn().Style.Border.RightBorder = XLBorderStyleValues.Thick;
+                        // rngTable.FirstRow().Style.Border.TopBorder = XLBorderStyleValues.Thick;
+                        // rngTable.LastRow().Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+
+                        //Adjust column widths to their content
+                        wb.Worksheet(usrName).Columns(2, 51).AdjustToContents();
+                    }
+                    wb.Worksheet(usrName).Cell("B"+i).Value = item.Match.Date;
+                    wb.Worksheet(usrName).Cell("C" + i).Value = item.Match.Local;
+                    wb.Worksheet(usrName).Cell("D" + i).Value = item.LocalGoals;
+                    wb.Worksheet(usrName).Cell("E" + i).Value = item.VisitorGoals;
+                    wb.Worksheet(usrName).Cell("F" + i).Value = item.Match.Visitor;
+                    i++;
+                }
+                
+                string folderPath = "C:\\Uni-Quiniela\\";
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                try { wb.SaveAs(folderPath + "UQ_Results_All.xlsx"); } catch (Exception e) { }
+                
+            }
+            Response.Redirect("/");
+        }
 
 
     }
